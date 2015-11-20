98c2af4ce386c74a49e3b12d40746dcc356f530d
Samuel Ainsworth <samuel_ainsworth@brown.edu>
2015-11-20 15:55:40 -0500

    Added SelectLazyGreedy

--- pyDPMP/particleselection.py
+++ pyDPMP/particleselection.py
@@ -27,7 +27,7 @@ class SelectDiverse(ParticleSelectionScheme):
 
       # Select first particle
       b = map_state[t]
-      Mhat = Psi[:,b]                                       # (S,)
+      Mhat = Psi[:, b]                                      # (S,)
       delta = Mstar - Mhat                                  # (S,)
       I_accept[t].append(b)
 
@@ -43,9 +43,58 @@ class SelectDiverse(ParticleSelectionScheme):
         I_accept[t].append(b)
 
         # Update message approximation
-        Mhat = np.maximum(Mhat, Psi[:,b])
+        Mhat = np.maximum(Mhat, Psi[:, b])
         delta = Mstar - Mhat
     return I_accept
 
 class SelectLazyGreedy(ParticleSelectionScheme):
-  pass
+  def select(self, mrf, map_state, msg_passing, msg, x_aug, nSelect, node_pot, \
+      edge_pot, temp):
+    """Run particle selection from the ICML 2015 paper (dpmpsum/LazyGreedy)."""
+    nStates = {v: len(node_pot[v]) for v in mrf.nodes}
+
+    I_accept = {v: [] for v in mrf.nodes}
+    for v in mrf.nodes:
+      # Build the "stacked" message foundations matrix (S x T)
+      M_t = np.vstack([msg_passing.message_foundation(node_pot, edge_pot, msg, \
+                           nStates, v, s).T
+                       for s in neighbors(mrf, v)])
+
+      # logMstar = np.max(M_t, axis=1)                        # (S,)
+      logZ = 1.0 / temp * np.max(M_t)                       # scalar
+      # Mstar = np.exp(1.0 / temp * logMstar - logZ)          # (S,)
+      Psi = np.exp(1.0 / temp * M_t - logZ)                 # (S, T)
+
+      # Select first particle
+      delta = np.sum(Psi, axis=0)
+      b = map_state[v]
+      I_accept[v].append(b)
+      Mhat = Psi[:, b]
+
+      deltaMax = np.inf
+      while (len(I_accept[v]) < nSelect[v]) and (deltaMax > 0):
+        # Get unused particles
+        b_used = set(I_accept[v])
+        b_unused = [bb for bb in range(nStates[v]) if bb not in b_used]
+
+        # Pick next particle
+        stale = []
+        b_max = None
+        for _ in range(len(b_unused) + 1):
+          # Pick highest margin
+          idx_max = np.argmax(delta[b_unused])
+          deltaMax = delta[b_unused][idx_max]
+          b_max = b_unused[idx_max]
+          if b_max in stale:
+            break
+
+          # Recompute margin
+          delta[b_max] = np.sum(np.maximum(Mhat, Psi[:, b_max])) - np.sum(Mhat)
+          stale.append(b_max)
+
+        # Add particle
+        if deltaMax > 0:
+          I_accept[v].append(b_max)
+          Mhat = np.maximum(Mhat, Psi[:, b_max])
+
+    return I_accept
--- tests/test_dpmp_infer.py
+++ tests/test_dpmp_infer.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from pyDPMP.messagepassing import MaxSumMP
 from pyDPMP.mrf import MRF
-from pyDPMP.particleselection import SelectDiverse
+from pyDPMP.particleselection import SelectDiverse, SelectLazyGreedy
 from pyDPMP.proposals import random_walk_proposal_1d
 from pyDPMP.util import seeded
 from pyDPMP import DPMP_infer
@@ -85,6 +85,94 @@ def test_dpmp_infer_nAugmented_int():
     return info['x_aug']
 
   xMAP, _, stats = DPMP_infer(mrf, x0, nParticles, proposal, SelectDiverse(),
+                              MaxSumMP(mrf), nAugmented=nAugmented,
+                              max_iters=50, callback=callback)
+
+  assert xMAP == {0: 0.0, 1: 0.0}
+  assert stats['converged'] == True
+  assert all([len(stats['callback_results'][it][v]) == nAugmented
+              for it in range(len(stats['callback_results']))
+              for v in mrf.nodes])
+
+##############################
+
+@seeded
+def test_dpmp_infer_lg():
+  """Test DPMP when the true MAP is in x0. Final MAP should the true MAP."""
+  mrf = MRF([0, 1], [(0, 1)],
+            lambda _1, x: -(x ** 2),
+            lambda _1, _2, x, y: -((x - y) ** 2))
+  x0 = {0: [0.0], 1: [0.0]}
+  nParticles = 5
+
+  def proposal(mrf, nParticlesAdd, _):
+    return {v: list(100 * np.random.randn(nParticlesAdd[v])) for v in mrf.nodes}
+
+  xMAP, _, stats = DPMP_infer(mrf, x0, nParticles, proposal, SelectLazyGreedy(),
+                              MaxSumMP(mrf), max_iters=50)
+
+  assert xMAP == {0: 0.0, 1: 0.0}
+  assert stats['converged'] == True
+
+@seeded
+def test_dpmp_infer_rw_prop_1d_lg():
+  """Test DPMP when the true MAP is in x0. Final MAP should the true MAP."""
+  mrf = MRF([0, 1], [(0, 1)],
+            lambda _1, x: -(x ** 2),
+            lambda _1, _2, x, y: -((x - y) ** 2))
+  x0 = {0: [0.0], 1: [0.0]}
+  nParticles = 5
+
+  prop = random_walk_proposal_1d(10)
+
+  xMAP, _, stats = DPMP_infer(mrf, x0, nParticles, prop, SelectLazyGreedy(),
+                              MaxSumMP(mrf), max_iters=50)
+
+  assert xMAP == {0: 0.0, 1: 0.0}
+  assert stats['converged'] == True
+
+@seeded
+def test_dpmp_infer_callback_lg():
+  """Test that DPMP callback is called."""
+  mrf = MRF([0, 1], [(0, 1)],
+            lambda _1, x: -(x ** 2),
+            lambda _1, _2, x, y: -((x - y) ** 2))
+  x0 = {0: [0.0], 1: [0.0]}
+  nParticles = 5
+
+  def proposal(mrf, nParticlesAdd, _):
+    return {v: list(100 * np.random.randn(nParticlesAdd[v])) for v in mrf.nodes}
+
+  called = [False]
+  def callback(info):
+    called[0] = True
+    return info['iter']
+
+  _, _, stats = DPMP_infer(mrf, x0, nParticles, proposal, SelectLazyGreedy(),
+                           MaxSumMP(mrf), max_iters=50, callback=callback)
+
+  assert called[0] == True
+  assert stats['converged'] == True
+  assert stats['callback_results'][-1] == stats['last_iter']
+
+@seeded
+def test_dpmp_infer_nAugmented_int_lg():
+  """Test DPMP with nAugmented = 5."""
+
+  mrf = MRF([0, 1], [(0, 1)],
+            lambda _1, x: -(x ** 2),
+            lambda _1, _2, x, y: -((x - y) ** 2))
+  x0 = {0: [0.0], 1: [0.0]}
+  nParticles = 2
+  nAugmented = 5
+
+  def proposal(mrf, nParticlesAdd, _):
+    return {v: list(100 * np.random.randn(nParticlesAdd[v])) for v in mrf.nodes}
+
+  def callback(info):
+    return info['x_aug']
+
+  xMAP, _, stats = DPMP_infer(mrf, x0, nParticles, proposal, SelectLazyGreedy(),
                               MaxSumMP(mrf), nAugmented=nAugmented,
                               max_iters=50, callback=callback)
 
