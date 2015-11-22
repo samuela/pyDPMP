036e342bceb5649e9ccc429efb3261acabee664b
Samuel Ainsworth <samuel_ainsworth@brown.edu>
2015-11-21 21:43:26 -0500

    Add random rho tests

--- tests/test_trw.py
+++ tests/test_trw.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from pyDPMP.messagepassing import TreeReweightedMP, decode_MAP_states
 from pyDPMP.mrf import calc_potentials
 from pyDPMP.util import set_seed
@@ -34,23 +36,36 @@ def test_trw_trees():
     mrf = random_tree_mrf(10)
     yield check_trw_tree, mrf
 
-def check_trw_bound_general(mrf):
+def check_trw_bound_general(mrf, rho):
   x = {v: [0, 1] for v in mrf.nodes}
   node_pot, edge_pot = calc_potentials(mrf, x)
 
   # Pick random edge appearances
-  rho = {(s, t): 0.5 for s in mrf.nodes for t in mrf.nodes}
   trw = TreeReweightedMP(mrf, rho)
   _, stats = trw.messages(node_pot, edge_pot, calc_logP=True)
 
   assert all([logP <= bound + 1e-10
               for (logP, bound) in zip(stats['logP'], stats['logPbound'])])
 
-def test_trw_general():
+def test_trw_general_half_rho():
   """Test that TreeReweightedMP works on random (general) graphs and the bound
-  works properly."""
+  works properly. rho = 0.5"""
   set_seed(0)
 
   for _ in range(100):
     mrf = random_mrf(5, 0.5)
-    yield check_trw_bound_general, mrf
+    rho = {(s, t): 0.5 for s in mrf.nodes for t in mrf.nodes}
+    yield check_trw_bound_general, mrf, rho
+
+def test_trw_general_random_rho():
+  """Test that TreeReweightedMP works on random (general) graphs and the bound
+  works properly. rho is random"""
+  set_seed(0)
+
+  for _ in range(100):
+    V = 5
+    mrf = random_mrf(V, 0.5)
+    rho_mat = np.random.rand(V, V)
+    rho = {(s, t): rho_mat[min(s, t), max(s, t)]
+           for s in mrf.nodes for t in mrf.nodes}
+    yield check_trw_bound_general, mrf, rho
