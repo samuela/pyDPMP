d1d2bf9e78aa09b18cebf114846e4790633a1d8f
Samuel Ainsworth <samuel_ainsworth@brown.edu>
2016-02-14 14:16:05 -0500

    transition lazy greedy particle selection

--- pyDPMP/particleselection.py
+++ pyDPMP/particleselection.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from .mrf import neighboring_factors
 
-def message_foundation(mrf, msg_passing, pots, t):
+def message_foundation(mrf, msg_passing, msgs, pots, t):
   mats = []
   for fid in neighboring_factors(mrf, t):
     # Messages from all other factors (T,)
@@ -35,27 +35,7 @@ class SelectDiverse(ParticleSelectionScheme):
     I_accept = {v: [] for v in mrf.nodes}
     for t in mrf.nodes:
       # Build the "stacked" message foundations matrix (S x T)
-      mats = []
-      for fid in neighboring_factors(mrf, t):
-        # Messages from all other factors (T,)
-        incoming_msg = msg_passing.vf_message(pots, msgs, t, fid)
-
-        factor = mrf.factors[fid]
-        pot = pots[fid]
-
-        # The axis of the pot matrix corresponding to t
-        ax_t = factor.nodes.index(t)
-
-        # Roll the ax_t axis to be the first
-        rolled_pot = np.rollaxis(pot, ax_t)
-
-        # Flatten out all other axes (T x <whatever>)
-        flat_pot = rolled_pot.reshape((rolled_pot.shape[0], -1))
-
-        # Calculate final message foundation matrix and add to mats
-        mats.append(flat_pot.T + incoming_msg.reshape((1, len(incoming_msg))))
-
-      M_t = np.vstack(mats)
+      M_t = message_foundation(mrf, msg_passing, msgs, pots, t)
 
       logMstar = np.max(M_t, axis=1)                        # (S,)
       logZ = 1.0 / temp * np.max(M_t)                       # scalar
@@ -84,53 +64,49 @@ class SelectDiverse(ParticleSelectionScheme):
         delta = Mstar - Mhat
     return I_accept
 
-# class SelectLazyGreedy(ParticleSelectionScheme):
-#   def select(self, mrf, map_state, msg_passing, msgs, nSelect, pots, temp):
-#     """Run particle selection from the ICML 2015 paper (dpmpsum/LazyGreedy)."""
-#     nStates = {v: len(node_pot[v]) for v in mrf.nodes}
-#
-#     I_accept = {v: [] for v in mrf.nodes}
-#     for v in mrf.nodes:
-#       # Build the "stacked" message foundations matrix (S x T)
-#       M_t = np.vstack([msg_passing.message_foundation(node_pot, edge_pot, msg, \
-#                            nStates, v, s).T
-#                        for s in neighbors(mrf, v)])
-#
-#       # logMstar = np.max(M_t, axis=1)                        # (S,)
-#       logZ = 1.0 / temp * np.max(M_t)                       # scalar
-#       # Mstar = np.exp(1.0 / temp * logMstar - logZ)          # (S,)
-#       Psi = np.exp(1.0 / temp * M_t - logZ)                 # (S, T)
-#
-#       # Select first particle
-#       delta = np.sum(Psi, axis=0)
-#       b = map_state[v]
-#       I_accept[v].append(b)
-#       Mhat = Psi[:, b]
-#
-#       deltaMax = np.inf
-#       while (len(I_accept[v]) < nSelect[v]) and (deltaMax > 0):
-#         # Get unused particles
-#         b_used = set(I_accept[v])
-#         b_unused = [bb for bb in range(nStates[v]) if bb not in b_used]
-#
-#         # Pick next particle
-#         stale = []
-#         b_max = None
-#         for _ in range(len(b_unused) + 1):
-#           # Pick highest margin
-#           idx_max = np.argmax(delta[b_unused])
-#           deltaMax = delta[b_unused][idx_max]
-#           b_max = b_unused[idx_max]
-#           if b_max in stale:
-#             break
-#
-#           # Recompute margin
-#           delta[b_max] = np.sum(np.maximum(Mhat, Psi[:, b_max])) - np.sum(Mhat)
-#           stale.append(b_max)
-#
-#         # Add particle
-#         if deltaMax > 0:
-#           I_accept[v].append(b_max)
-#           Mhat = np.maximum(Mhat, Psi[:, b_max])
-#
-#     return I_accept
+class SelectLazyGreedy(ParticleSelectionScheme):
+  def select(self, mrf, nStates, map_state, msg_passing, msgs, nSelect, pots, temp):
+    """Run particle selection from the ICML 2015 paper (dpmpsum/LazyGreedy)."""
+    I_accept = {v: [] for v in mrf.nodes}
+    for v in mrf.nodes:
+      # Build the "stacked" message foundations matrix (S x T)
+      M_t = message_foundation(mrf, msg_passing, msgs, pots, v)
+
+      # logMstar = np.max(M_t, axis=1)                        # (S,)
+      logZ = 1.0 / temp * np.max(M_t)                       # scalar
+      # Mstar = np.exp(1.0 / temp * logMstar - logZ)          # (S,)
+      Psi = np.exp(1.0 / temp * M_t - logZ)                 # (S, T)
+
+      # Select first particle
+      delta = np.sum(Psi, axis=0)
+      b = map_state[v]
+      I_accept[v].append(b)
+      Mhat = Psi[:, b]
+
+      deltaMax = np.inf
+      while (len(I_accept[v]) < nSelect[v]) and (deltaMax > 0):
+        # Get unused particles
+        b_used = set(I_accept[v])
+        b_unused = [bb for bb in range(nStates[v]) if bb not in b_used]
+
+        # Pick next particle
+        stale = []
+        b_max = None
+        for _ in range(len(b_unused) + 1):
+          # Pick highest margin
+          idx_max = np.argmax(delta[b_unused])
+          deltaMax = delta[b_unused][idx_max]
+          b_max = b_unused[idx_max]
+          if b_max in stale:
+            break
+
+          # Recompute margin
+          delta[b_max] = np.sum(np.maximum(Mhat, Psi[:, b_max])) - np.sum(Mhat)
+          stale.append(b_max)
+
+        # Add particle
+        if deltaMax > 0:
+          I_accept[v].append(b_max)
+          Mhat = np.maximum(Mhat, Psi[:, b_max])
+
+    return I_accept
